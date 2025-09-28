cf308797f6cbe85f945472aa78a9c865ca45b0c5
Sadia1609 <sadia.rahman160920@gmail.com>
2025-09-28 16:20:52 +0600

    added dom in js file

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const categoryContainer = document.getElementById("category-container");
+
+  const plantContainer = document.getElementById("plant-container");
+
+  const spinner = document.getElementById("spinner");
+
+  const cartContainer = document.getElementById("cart");
+
+  const totalPrice = document.getElementById("total-price");
+
+  const modal = document.getElementById("detail-modal");
+
+  const detailContent = document.getElementById("detail-content");
+
+  const closeModal = document.getElementById("close-modal");
+
+})
